b1c80d6eebd26d1effe44debea46434f53c7a640
oussamasmihrou <oussamasmihrou@gmail.com>
2021-12-29 02:29:40 +0100

    upgrading the app style and functionalities

---
+++ Dash web app/app.py
@@ -0,0 +1,7 @@
+import dash
+
+# meta_tags are required for the app layout to be mobile responsive
+                
+app = dash.Dash(__name__,suppress_callback_exceptions=True)
+server = app.server
+app.title = "DeepSearch Lab"
